0d0e5415f6cc6f78c96fb305f0a8d2653a5698ff
DevY <hanys1042@gmail.com>
2018-06-26 16:51:58 +0900

    어플리케이션 시작시 데이터베이스 디렉터리 생성

--- src/main/java/devy/kave/server/KaveServerApplication.java
+++ src/main/java/devy/kave/server/KaveServerApplication.java
@@ -1,12 +1,28 @@
 package devy.kave.server;
 
+import devy.kave.server.db.EnvironmentBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 @SpringBootApplication
 public class KaveServerApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(KaveServerApplication.class);
+
 	public static void main(String[] args) {
+
+        File file = new File(EnvironmentBuilder.defaultDbDir);
+        if(!file.exists()) {
+            file.mkdir();
+            logger.info("Created bdb directory");
+        } else {
+            logger.info("Already Created bdb directory");
+        }
+
 		SpringApplication.run(KaveServerApplication.class, args);
 	}
 }
--- src/test/java/devy/kave/server/KaveServerApplicationTests.java
+++ src/test/java/devy/kave/server/KaveServerApplicationTests.java
@@ -2,6 +2,8 @@ package devy.kave.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class KaveServerApplicationTests {
 
-	@Test
+    private static Logger logger = LoggerFactory.getLogger(KaveServerApplication.class);
+
+    public KaveServerApplicationTests() {
+//        File file = new File(EnvironmentSingleton.DB_DIR_PATH);
+//        if(!file.exists()) {
+//            file.mkdir();
+//            logger.info("Created bdb directory");
+//        } else {
+//            logger.info("Already Created bdb directory");
+//        }
+    }
+
+    @Test
 	public void contextLoads() {
+
 	}
 
 }
