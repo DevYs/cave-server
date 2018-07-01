4151782e6755c04fa7e4d0042794247e415913ce
DevY <hanys1042@gmail.com>
2018-07-01 15:55:13 +0900

    db mapper의 공통 메서드를 위한 인터페이스

---
+++ src/main/java/devy/kave/server/db/mapper/Mapper.java
@@ -0,0 +1,22 @@
+package devy.kave.server.db.mapper;
+
+import com.sleepycat.bind.tuple.MarshalledTupleKeyEntity;
+import com.sleepycat.collections.*;
+
+public interface Mapper {
+
+    boolean add(MarshalledTupleKeyEntity entity);
+
+    Object remove(long key);
+
+    Object mod(MarshalledTupleKeyEntity entity);
+
+    StoredValueSet set();
+
+    StoredSortedValueSet sortedSet();
+
+    StoredMap map();
+
+    StoredSortedMap sortedMap();
+
+}
