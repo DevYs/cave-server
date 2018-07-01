a5fcb01e64a527561394b6516ec1ea5dab11d654
DevY <hanys1042@gmail.com>
2018-07-01 14:01:08 +0900

    User 정보 획득에 대한 코드 추가

--- src/main/java/devy/kave/server/controller/IndexController.java
+++ src/main/java/devy/kave/server/controller/IndexController.java
@@ -1,8 +1,5 @@
 package devy.kave.server.controller;
 
-import com.sleepycat.collections.StoredValueSet;
-import devy.kave.server.db.DatabaseKeyCreator;
-import devy.kave.server.db.model.Channel;
 import devy.kave.server.db.service.ChannelService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Iterator;
+import java.security.Principal;
 
 @Controller
 public class IndexController {
@@ -31,20 +28,8 @@ public class IndexController {
     }
 
     @GetMapping("/index")
-    public String index(Model model) {
-
-        StoredValueSet channels = channelService.get();
-
-        Iterator iterator = channels.iterator();
-
-        while(iterator.hasNext()) {
-            Channel channel = (Channel) iterator.next();
-            logger.info(channel.toString());
-        }
-
-        model.addAttribute("test", "test");
-        model.addAttribute("channels", channels.iterator());
-
+    public String index(Principal principal, Model model) {
+        logger.info(">>>> user : " + principal.getName());
         return "index";
     }
 
