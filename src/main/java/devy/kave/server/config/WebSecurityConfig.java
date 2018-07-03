bc3e958fe499aa4465e8a3d37fdcb40dbc9649ad
DevY <hanys1042@gmail.com>
2018-07-03 15:19:55 +0900

    InMemoryAuth 제거

--- src/main/java/devy/kave/server/config/WebSecurityConfig.java
+++ src/main/java/devy/kave/server/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package devy.kave.server.config;
 
+import devy.kave.server.db.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +10,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -32,6 +26,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+    @Autowired
+    private UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -56,14 +53,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("test")
-                .password(passwordEncoder.encode("1234"))
-                .roles("USER")
-        .and()
-                .withUser("admin")
-                .password(passwordEncoder.encode("1234"))
-                .roles("USER", "ADMIN");
-
+        auth
+            .userDetailsService(this.userService)
+            .passwordEncoder(this.passwordEncoder);
     }
 }
