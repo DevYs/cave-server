72752ae923de87dd20b02a195178814b5e6d0ac9
DevY <hanys1042@gmail.com>
2018-07-05 14:27:42 +0900

    사용자 검색 기능

--- src/main/java/devy/kave/server/controller/SearchController.java
+++ src/main/java/devy/kave/server/controller/SearchController.java
@@ -1,15 +1,46 @@
 package devy.kave.server.controller;
 
+import devy.kave.server.db.model.Channel;
+import devy.kave.server.db.model.Contents;
+import devy.kave.server.db.service.ChannelService;
+import devy.kave.server.db.service.ContentsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class SearchController {
 
+    private final Logger logger = LoggerFactory.getLogger(SearchController.class);
+
+    @Autowired
+    private ChannelService channelService;
+
+    @Autowired
+    private ContentsService contentsService;
+
     @GetMapping("/search")
-    public String search(Model model) {
+    public String search(@RequestParam(defaultValue = "1") int pageNo, String searchWord, long channelNo, Model model) {
+        List<Contents> contents = contentsService.contentsList(pageNo, searchWord, channelNo);
+
+
+        boolean isChannel = channelNo != 0L;
+        model.addAttribute("searchChannel", isChannel ? channelService.getChannel(channelNo) : new Channel(0L, "전체"));
+        model.addAttribute("searchWord", searchWord);
+        model.addAttribute("contentsList", contents);
+        model.addAttribute("contentsSize", contents.size());
         return "search";
+    }
+
+    @RequestMapping(value = "/api/search", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Contents> search(@RequestParam(defaultValue = "1") int pageNo, String searchWord, long channelNo) {
+        return contentsService.contentsList(pageNo, searchWord, channelNo);
     }
 
 }
---
+++ src/main/resources/static/js/search.js
@@ -0,0 +1,42 @@
+var page = 1;
+
+var contentsTemp = "<div class='idx-thumb col-xs-4 col-sm-3 col-md-2'>" +
+                    "<a href='/contents?contentsNo=${contentsNo})}'>" +
+                        "<img class='img-responsive' src='${contentsPosterUrl}' alt='${alt}' />" +
+                    "</a>" +
+                    "<a class='idx-thumb-tl' href='/contents?contentsNo=${contentsNo})}'>${contentsName}</a>" +
+                    "<a class='idx-thumb-tl' href='/contents?contentsNo=${contentsNo})}'>${releaseDate}</a>" +
+                "</div>";
+
+$(document).ready(function() {
+    $("#next").click(function() {
+        $(this).attr("disabled", "disabled");
+
+        page = page + 1;
+
+        $.ajax({
+            url : "/api/search?pageNo=" + page + "&searchWord=" + $("#searchWord").val() + "&channelNo=" + $("#channelNo").val(),
+            success : function(contentsList){
+                if(contentsList.length < 1) {
+                    $("#next").hide();
+                    return;
+                }
+
+                for(var i=0; i<contentsList.length; i++) {
+                    var c = contentsTemp
+                    .replace("${contentsNo}", contentsList[i].contentsNo)
+                    .replace("${contentsNo}", contentsList[i].contentsNo)
+                    .replace("${contentsNo}", contentsList[i].contentsNo)
+                    .replace("${contentsPosterUrl}", contentsList[i].contentsPosterUrl)
+                    .replace("${contentsName}", contentsList[i].contentsName)
+                    .replace("${releaseDate}", contentsList[i].releaseDate)
+                    .replace("${alt}", contentsList[i].contentsName + " 포스터 이미지");
+                    $("#contentsList").append(c);
+                }
+
+                $("#next").removeAttr("disabled");
+            }
+        })
+    });
+});
+
