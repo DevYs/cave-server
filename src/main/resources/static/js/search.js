474261782a4b84bb7443a7da81f6e4b1dd0502b3
DevY <hanys1042@gmail.com>
2018-07-06 14:22:16 +0900

    contentsNo 변수에 1,2,3 넘버링하여 치환

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,11 +1,11 @@
 var page = 1;
 
 var contentsTemp = "<div class='idx-thumb col-xs-4 col-sm-3 col-md-2'>" +
-                    "<a href='/contents?contentsNo=${contentsNo}'>" +
+                    "<a href='/contents?contentsNo=${contentsNo1}'>" +
                         "<img class='img-responsive' src='${contentsPosterUrl}' alt='${alt}' />" +
                     "</a>" +
-                    "<a class='idx-thumb-tl' href='/contents?contentsNo=${contentsNo}'>${contentsName}</a>" +
-                    "<a class='idx-thumb-tl' href='/contents?contentsNo=${contentsNo}'>${releaseDate}</a>" +
+                    "<a class='idx-thumb-tl' href='/contents?contentsNo=${contentsNo2}'>${contentsName}</a>" +
+                    "<a class='idx-thumb-tl' href='/contents?contentsNo=${contentsNo3}'>${releaseDate}</a>" +
                 "</div>";
 
 $(document).ready(function() {
@@ -23,10 +23,12 @@ $(document).ready(function() {
                 }
 
                 for(var i=0; i<contentsList.length; i++) {
+                    var contentsNo = contentsList[i].contentsNo;
+
                     var c = contentsTemp
-                    .replace("${contentsNo}", contentsList[i].contentsNo)
-                    .replace("${contentsNo}", contentsList[i].contentsNo)
-                    .replace("${contentsNo}", contentsList[i].contentsNo)
+                    .replace("${contentsNo1}", contentsNo)
+                    .replace("${contentsNo2}", contentsNo)
+                    .replace("${contentsNo3}", contentsNo)
                     .replace("${contentsPosterUrl}", contentsList[i].contentsPosterUrl)
                     .replace("${contentsName}", contentsList[i].contentsName)
                     .replace("${releaseDate}", contentsList[i].releaseDate)
@@ -38,5 +40,6 @@ $(document).ready(function() {
             }
         })
     });
+
 });
 
