eab6452468c542f77a712c5b91f7434349f90db3
DevY <hanys1042@gmail.com>
2018-07-06 15:13:12 +0900

    key에 의한 정렬을 위한 유틸리티

--- src/main/java/devy/kave/server/util/Sort.java
+++ src/main/java/devy/kave/server/util/Sort.java
@@ -1,20 +1,46 @@
 package devy.kave.server.util;
 
 import com.sleepycat.bind.tuple.MarshalledTupleKeyEntity;
+import com.sleepycat.bind.tuple.TupleOutput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
 
 public class Sort {
 
-    public static <E extends MarshalledTupleKeyEntity> List<E> reverse(List<E> list) {
-        List<E> temp = new ArrayList<>();
-        int startIndex = list.size() - 1;
-        int endIndex = 0;
-        for(int i=startIndex; i>=endIndex; i--) {
-            temp.add(list.get(i));
+    private static final Logger logger = LoggerFactory.getLogger(Sort.class);
+
+    public SortDescending descending() {
+        return new SortDescending();
+    }
+
+    public SortAscending ascending() {
+        return new SortAscending();
+    }
+
+    private class SortDescending implements Comparator<MarshalledTupleKeyEntity> {
+        @Override
+        public int compare(MarshalledTupleKeyEntity o1, MarshalledTupleKeyEntity o2) {
+            Long o1Key = toLong(o1);
+            Long o2Key = toLong(o2);
+            return o1Key < o2Key ? 1 : -1;
         }
-        return temp;
+    }
+
+    private class SortAscending implements Comparator<MarshalledTupleKeyEntity> {
+        @Override
+        public int compare(MarshalledTupleKeyEntity o1, MarshalledTupleKeyEntity o2) {
+            Long o1Key = toLong(o1);
+            Long o2Key = toLong(o2);
+            return o1Key > o2Key ? 1 : -1;
+        }
+    }
+
+    private Long toLong(MarshalledTupleKeyEntity o) {
+        TupleOutput keyOutput = new TupleOutput();
+        o.marshalPrimaryKey(keyOutput);
+        return Long.valueOf(keyOutput.toString().substring(0, 17));
     }
 
 }
