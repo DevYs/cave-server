cb2b9280ddbbb19e2d1e2945cd3c0cd8172777b7
DevY <hanys1042@gmail.com>
2018-07-06 14:23:01 +0900

    db 키의 타입을 String 타입으로 변경

--- src/main/java/devy/kave/server/db/DatabaseKeyCreator.java
+++ src/main/java/devy/kave/server/db/DatabaseKeyCreator.java
@@ -8,6 +8,9 @@ public class DatabaseKeyCreator {
     private static final SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmssSSS");
 
     public static final String createKey() {
-        return String.valueOf(System.currentTimeMillis());
+        long lt = System.currentTimeMillis();
+        String st = format.format(new Date(lt));
+        long l = Long.valueOf(st);
+        return String.valueOf(l);
     }
 }
