2b9678cca61fa59e7c7b435569048e3d85f79074
DevY <hanys1042@gmail.com>
2018-07-06 16:31:37 +0900

    사용자 영상 재생 화면

--- src/main/java/devy/kave/server/controller/PlayController.java
+++ src/main/java/devy/kave/server/controller/PlayController.java
@@ -1,14 +1,31 @@
 package devy.kave.server.controller;
 
+import devy.kave.server.db.model.Contents;
+import devy.kave.server.db.model.Video;
+import devy.kave.server.db.service.ContentsService;
+import devy.kave.server.db.service.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collection;
+
 @Controller
 public class PlayController {
 
+    @Autowired
+    private ContentsService contentsService;
+
+    @Autowired
+    private VideoService videoService;
+
     @GetMapping("/play")
-    public String play(Model model) {
+    public String play(String videoNo, Model model) {
+        Video video = videoService.getVideo(videoNo);
+        model.addAttribute("watchingVideo", video);
+        model.addAttribute("contents", contentsService.getContents(video.getContentsNo()));
+        model.addAttribute("videoList", contentsService.videoList(video.getContentsNo()));
         return "play";
     }
 
