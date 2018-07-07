6b5b8bac000cb1ea0cb45cff657eb659674afc48
DevY <hanys1042@gmail.com>
2018-07-07 09:26:59 +0900

    Watching, Deck 디비 추가

--- src/main/java/devy/kave/server/db/QueryMap.java
+++ src/main/java/devy/kave/server/db/QueryMap.java
@@ -11,6 +11,8 @@ public class QueryMap extends HashMap<String, Query> {
         contents();
         user();
         video();
+        watching();
+        deck();
     }
 
     private void channel() {
@@ -21,7 +23,7 @@ public class QueryMap extends HashMap<String, Query> {
         put(Contents.DB_CONTENTS, new Query().setDbName(Contents.DB_CONTENTS).setKeyClass(ContentsKey.class).setValueBaseClass(Contents.class));
         put(Contents.INDEX_CONTENTS_CHANNEL_NO, new Query()
                 .setDbName(Contents.INDEX_CONTENTS_CHANNEL_NO)
-                .setKeyClass(String.class)
+                .setKeyClass(ChannelKey.class)
                 .setValueBaseClass(Contents.class)
                 .setPrimaryDbName(Contents.DB_CONTENTS)
                 .setForeignKeyDbName(Channel.DB_CHANNEL)
@@ -32,7 +34,7 @@ public class QueryMap extends HashMap<String, Query> {
         put(Video.DB_VIDEO, new Query().setDbName(Video.DB_VIDEO).setKeyClass(VideoKey.class).setValueBaseClass(Video.class));
         put(Video.INDEX_VIDEO_CONTENTS_NO, new Query()
                 .setDbName(Video.INDEX_VIDEO_CONTENTS_NO)
-                .setKeyClass(String.class)
+                .setKeyClass(ContentsKey.class)
                 .setValueBaseClass(Video.class)
                 .setPrimaryDbName(Video.DB_VIDEO)
                 .setForeignKeyDbName(Contents.DB_CONTENTS)
@@ -47,6 +49,42 @@ public class QueryMap extends HashMap<String, Query> {
                 .setValueBaseClass(User.class)
                 .setKeyName(User.KEY_USER_USER_ID)
                 .setPrimaryDbName(User.DB_USER));
+    }
+
+    private void watching() {
+        put(Watching.DB_WATCHING, new Query().setDbName(Watching.DB_WATCHING).setKeyClass(WatchingKey.class).setValueBaseClass(Watching.class));
+        put(Watching.INDEX_WATCHING_USER_NO, new Query()
+                .setDbName(Watching.INDEX_WATCHING_USER_NO)
+                .setKeyClass(UserKey.class)
+                .setValueBaseClass(Watching.class)
+                .setKeyName(Watching.KEY_WATCHING_USER_NO)
+                .setForeignKeyDbName(User.DB_USER)
+                .setPrimaryDbName(Watching.DB_WATCHING));
+        put(Watching.INDEX_WATCHING_VIDEO_NO, new Query()
+                .setDbName(Watching.INDEX_WATCHING_VIDEO_NO)
+                .setKeyClass(VideoKey.class)
+                .setValueBaseClass(Watching.class)
+                .setKeyName(Watching.KEY_WATCHING_VIDEO_NO)
+                .setForeignKeyDbName(Video.DB_VIDEO)
+                .setPrimaryDbName(Watching.DB_WATCHING));
+    }
+
+    private void deck() {
+        put(Deck.DB_DECK, new Query().setDbName(Deck.DB_DECK).setKeyClass(DeckKey.class).setValueBaseClass(Deck.class));
+        put(Deck.INDEX_DECK_USER_NO, new Query()
+                .setDbName(Deck.INDEX_DECK_USER_NO)
+                .setKeyClass(UserKey.class)
+                .setValueBaseClass(Deck.class)
+                .setKeyName(Deck.KEY_DECK_USER_NO)
+                .setForeignKeyDbName(User.DB_USER)
+                .setPrimaryDbName(Deck.DB_DECK));
+        put(Deck.DB_DECK, new Query()
+                .setDbName(Deck.INDEX_DECK_VIDEO_NO)
+                .setKeyClass(VideoKey.class)
+                .setValueBaseClass(Deck.class)
+                .setKeyName(Deck.KEY_DECK_VIDEO_NO)
+                .setForeignKeyDbName(Video.DB_VIDEO)
+                .setPrimaryDbName(Deck.DB_DECK));
     }
 
 }
