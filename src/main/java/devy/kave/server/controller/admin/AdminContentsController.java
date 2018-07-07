c57b96810b8433f8e678194f29ee5fac84f475fa
DevY <hanys1042@gmail.com>
2018-07-07 13:39:36 +0900

    테스트 데이터 제거

--- src/main/java/devy/kave/server/controller/admin/AdminContentsController.java
+++ src/main/java/devy/kave/server/controller/admin/AdminContentsController.java
@@ -96,33 +96,4 @@ public class AdminContentsController {
         return "redirect:/admin/contents";
     }
 
-    @GetMapping("/admin/contents/data")
-    public String data() {
-        String[] a = {"20180707091229904", "20180707091234599", "20180707091241303", "20180707091249074", "20180707091252737"};
-        String[] b = {"a", "b", "c", "d", "e", "f", "g", "가", "나", "다", "참", "햄", "피", "라", "치"};
-
-        Contents contents = contentsService.getContents("20180707091441554");
-        for(int i=0; i<150; i++) {
-            Contents c = new Contents();
-            c.setChannelNo(a[i/30]);
-            c.setContentsNo(DatabaseKeyCreator.createKey());
-            c.setContentsName(b[i/10] + contents.getContentsName() + (i+1));
-            c.setStory(contents.getStory());
-            c.setActor(contents.getActor());
-            c.setContentsPosterUrl(contents.getContentsPosterUrl());
-            c.setDirector(contents.getDirector());
-            c.setNation(contents.getNation());
-            c.setGenre(contents.getGenre());
-            c.setReleaseDate(contents.getReleaseDate());
-            c.setRunningTime(contents.getRunningTime());
-            boolean add = contentsService.add(c);
-            if(add) {
-                logger.info(c.toString());
-            }
-        }
-
-        return "redirect:/admin/contents";
-    }
-
-
 }
