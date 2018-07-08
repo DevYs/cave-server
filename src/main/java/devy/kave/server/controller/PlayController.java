fbebf994a291861558b42148137d4d946597d7a2
DevY <hanys1042@gmail.com>
2018-07-08 11:45:16 +0900

    기존에 PlayController에서 사용하던 Service들의 기능을 PlayService로 옮김

--- src/main/java/devy/kave/server/controller/PlayController.java
+++ src/main/java/devy/kave/server/controller/PlayController.java
@@ -1,22 +1,17 @@
 package devy.kave.server.controller;
 
 import devy.kave.server.db.model.Contents;
-import devy.kave.server.db.model.User;
 import devy.kave.server.db.model.Video;
-import devy.kave.server.db.model.Watching;
-import devy.kave.server.db.service.ContentsService;
-import devy.kave.server.db.service.UserService;
-import devy.kave.server.db.service.VideoService;
-import devy.kave.server.db.service.WatchingService;
+import devy.kave.server.db.service.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
+import java.util.Collection;
 
 @Controller
 public class PlayController {
@@ -24,37 +19,24 @@ public class PlayController {
     private final Logger logger = LoggerFactory.getLogger(PlayController.class);
 
     @Autowired
-    private UserService userService;
-
-    @Autowired
-    private ContentsService contentsService;
-
-    @Autowired
-    private VideoService videoService;
-
-    @Autowired
-    private WatchingService watchingService;
+    private PlayService playService;
 
     @GetMapping("/play")
     public String play(Principal principal, String videoNo, Model model) {
-        logger.info(principal.getName());
-        User user = userService.getUserByUserId(principal.getName());
-        Video video = videoService.getVideo(videoNo);
-        Watching watching = watchingService.getWatching(user.getUserNo(), videoNo);
-        Contents contents = contentsService.getContents(video.getContentsNo());
+        Video video = playService.getVideo(videoNo);
+        boolean isWatching = playService.isWatching(principal.getName(), videoNo);
+        Contents contents = playService.getContents(video.getContentsNo());
+        Collection<Video> videoList = playService.videoList(video.getContentsNo());
 
-        boolean isWatching = watching != null;
-        if(!isWatching) {
-            boolean isAdded = watchingService.add(user.getUserNo(), video, contents);
-            if(isAdded) {
-                logger.info("'" + user.getUserId() + "'님 " + contents.getContentsName() + " " + video.getVideoName() + " 시작");
-            }
+        boolean isAdded = playService.add(principal.getName(), videoNo, contents, "0");
+        if(isAdded) {
+            logger.info("'" + principal.getName() + "'님 " + contents.getContentsName() + " " + video.getVideoName() + " 시작");
         }
 
         model.addAttribute("isWatching", isWatching);
         model.addAttribute("watchingVideo", video);
         model.addAttribute("contents", contents);
-        model.addAttribute("videoList", contentsService.videoList(video.getContentsNo()));
+        model.addAttribute("videoList", videoList);
 
         return "play";
     }
---
+++ src/main/java/devy/kave/server/db/service/PlayService.java
@@ -0,0 +1,79 @@
+package devy.kave.server.db.service;
+
+import devy.kave.server.db.mapper.ContentsMapper;
+import devy.kave.server.db.mapper.UserMapper;
+import devy.kave.server.db.mapper.VideoMapper;
+import devy.kave.server.db.mapper.WatchingMapper;
+import devy.kave.server.db.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
+@Service
+public class PlayService {
+
+    private final Logger logger = LoggerFactory.getLogger(PlayService.class);
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private VideoMapper videoMapper;
+
+    @Autowired
+    private WatchingMapper watchingMapper;
+
+    @Autowired
+    private ContentsMapper contentsMapper;
+
+    public Video getVideo(String videoNo) {
+        return (Video) videoMapper.map().duplicates(new VideoKey(videoNo)).iterator().next();
+    }
+
+    public boolean isWatching(String userId, String videoNo) {
+        return getWatching(userId, videoNo) != null;
+    }
+
+    public Watching getWatching(String userId, String videoNo) {
+        User user = (User) userMapper.mapByUserId().duplicates(userId).iterator().next();
+
+        Watching watching;
+        try {
+            WatchingKey watchingKey = new WatchingKey(user.getUserNo(), videoNo);
+            watching = (Watching) watchingMapper.sortedMap().duplicates(watchingKey).iterator().next();
+        } catch (NoSuchElementException e) {
+            watching = null;
+        }
+
+        return watching;
+    }
+
+    public boolean add(String userId, String videoNo, Contents watchingContents, String watchingTime) {
+        User user = (User) userMapper.mapByUserId().duplicates(userId).iterator().next();
+        Video video = (Video) videoMapper.map().duplicates(new VideoKey(videoNo)).iterator().next();
+        Watching watching = getWatching(user.getUserNo(), video.getVideoNo());
+
+        boolean isWatching = watching != null;
+        if(isWatching) {
+            watching.setWatchingTime(watchingTime);
+            Watching modifiedWatching = (Watching) watchingMapper.map().replace(watching.getWatchingKey(), watching);
+            return modifiedWatching.equals(watchingTime);
+        }
+
+        Watching newWatching = new Watching(user.getUserNo(), video.getVideoNo(), watchingContents, video, "0");
+        return watchingMapper.add(newWatching);
+    }
+
+    public Contents getContents(String contentsNo) {
+        return (Contents) contentsMapper.map().duplicates(new ContentsKey(contentsNo)).iterator().next();
+    }
+
+    public Collection<Video> videoList(String contentsNo) {
+        return videoMapper.sortedSetByContentsNo().duplicates(new ContentsKey(contentsNo));
+    }
+
+}
