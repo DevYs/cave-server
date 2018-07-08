761849260f5307b7c9e4311a9d4136b6c21768b5
DevY <hanys1042@gmail.com>
2018-07-08 11:59:44 +0900

    SearchController에서 사용하던 service의 기능을 SearchService로 옮김

--- src/main/java/devy/kave/server/controller/SearchController.java
+++ src/main/java/devy/kave/server/controller/SearchController.java
@@ -2,8 +2,7 @@ package devy.kave.server.controller;
 
 import devy.kave.server.db.model.Channel;
 import devy.kave.server.db.model.Contents;
-import devy.kave.server.db.service.ChannelService;
-import devy.kave.server.db.service.ContentsService;
+import devy.kave.server.db.service.SearchService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,17 +18,14 @@ public class SearchController {
     private final Logger logger = LoggerFactory.getLogger(SearchController.class);
 
     @Autowired
-    private ChannelService channelService;
-
-    @Autowired
-    private ContentsService contentsService;
+    private SearchService searchService;
 
     @GetMapping("/search")
     public String search(@RequestParam(defaultValue = "1") int pageNo, String searchWord, String channelNo, Model model) {
-        List<Contents> contents = contentsService.contentsList(pageNo, searchWord, channelNo);
+        List<Contents> contents = searchService.contentsList(pageNo, searchWord, channelNo);
 
         boolean isChannel = !channelNo.equals("0");
-        model.addAttribute("searchChannel", isChannel ? channelService.getChannel(channelNo) : new Channel("0", "전체"));
+        model.addAttribute("searchChannel", isChannel ? searchService.getChannel(channelNo) : new Channel("0", "전체"));
         model.addAttribute("searchWord", searchWord);
         model.addAttribute("contentsList", contents);
         model.addAttribute("contentsSize", contents.size());
@@ -39,7 +35,7 @@ public class SearchController {
     @RequestMapping(value = "/api/search", method = RequestMethod.GET)
     @ResponseBody
     public List<Contents> search(@RequestParam(defaultValue = "1") int pageNo, String searchWord, String channelNo) {
-        return contentsService.contentsList(pageNo, searchWord, channelNo);
+        return searchService.contentsList(pageNo, searchWord, channelNo);
     }
 
 }
