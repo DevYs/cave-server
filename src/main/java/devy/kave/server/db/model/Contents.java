66d88808a64e543f865a00620811da984ac674c6
DevY <hanys1042@gmail.com>
2018-07-12 13:12:37 +0900

    포스터 이미지는 nullable

--- src/main/java/devy/kave/server/db/model/Contents.java
+++ src/main/java/devy/kave/server/db/model/Contents.java
@@ -24,7 +24,6 @@ public class Contents implements Serializable, MarshalledTupleKeyEntity {
         this.contentsNo = contentsNo;
     }
 
-    @NotNull
     @Size(max = 2000)
     private String contentsPosterUrl;
 
