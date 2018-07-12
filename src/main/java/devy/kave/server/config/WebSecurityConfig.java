2709a1b5dd873815b4f8caa73831b8b80a8b2252
DevY <hanys1042@gmail.com>
2018-07-12 17:23:19 +0900

    js, css 인증받지 않아도 접근을 허용

--- src/main/java/devy/kave/server/config/WebSecurityConfig.java
+++ src/main/java/devy/kave/server/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -55,5 +56,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth
             .userDetailsService(this.userService)
             .passwordEncoder(this.passwordEncoder);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/js/**", "/css/**");
     }
 }
