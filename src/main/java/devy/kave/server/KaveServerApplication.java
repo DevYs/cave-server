3c96081e3adb226978d9f5edb87f0782da1b2d48
DevY <hanys1042@gmail.com>
2018-07-13 14:43:12 +0900

    Cave 서버 종료 기능 추가

--- src/main/java/devy/kave/server/KaveServerApplication.java
+++ src/main/java/devy/kave/server/KaveServerApplication.java
@@ -3,8 +3,10 @@ package devy.kave.server;
 import devy.kave.server.db.EnvironmentBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.io.File;
 
@@ -14,7 +16,6 @@ public class KaveServerApplication {
     private static Logger logger = LoggerFactory.getLogger(KaveServerApplication.class);
 
 	public static void main(String[] args) {
-
         File file = new File(EnvironmentBuilder.defaultDbDir);
         if(!file.exists()) {
             file.mkdir();
@@ -23,6 +24,13 @@ public class KaveServerApplication {
             logger.info("Already Created bdb directory");
         }
 
-		SpringApplication.run(KaveServerApplication.class, args);
-	}
+        SpringApplication.run(KaveServerApplication.class, args);
+
+    }
+
+    @Bean
+    public ExitCodeGenerator exitCodeGenerator() {
+        return () -> 42;
+    }
+
 }
