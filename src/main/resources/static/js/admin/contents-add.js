32bbace90f9b91a17343b254c0a3a159d4a968ec
DevY <hanys1042@gmail.com>
2018-07-28 17:08:26 +0900

    다음 영화에서 영화정보를 가져오도록 수정

--- src/main/resources/static/js/admin/contents-add.js
+++ src/main/resources/static/js/admin/contents-add.js
@@ -5,18 +5,21 @@ var ROLE_NAME_DIRECTOR = "감독";
 var ROLE_NAME_DIRECTOR_2 = "연출";
 var PEOPLE_SEPERATOR = ", ";
 
+var TAG_CONTENTS_ID = "{contentsId}";
+var TAG_TITLE = "{title}";
+var TAG_YEAR = "{year}";
+var TAG_ACTOR = "{actor}";
+
 var URL_CONTENTS = "http://devy.iptime.org:1043/contents";
 var URL_CONTENTS_LIST = "http://devy.iptime.org:1043/contents/list";
 //var URL_CONTENTS = "http://192.168.43.54:1043/contents";
 //var URL_CONTENTS_LIST = "http://192.168.43.54:1043/contents/list";
 var page = 1;
 
-var template = "<a href='#' id='{code}' class='list-group-item'>" +
-                    "<h4 class='list-group-item-heading'>{title} {titleEng} {year}</h4>" +
-                    "<p class='list-group-item-text'>{director} {genre} {nation}</p>" +
+var template = "<a href='#' id='" + TAG_CONTENTS_ID + "' class='list-group-item search-item'>" +
+                    "<h4 class='list-group-item-heading'>" + TAG_TITLE + " " + TAG_YEAR + "</h4>" +
+                    "<p class='list-group-item-text'>" + TAG_ACTOR + "</p>" +
                 "</a>";
-
-
 
 $(document).ready(function() {
     $("#searchBtn").on("click", function() {
@@ -75,58 +78,57 @@ function requestContentsList(direction) {
         {searchText: searchText, page: page},
         function(data) {
 
-            if(data.length < 1) {
-                if(direction == "next") {
-                    alert("마지막 페이지 입니다.");
-                    page = page - 1;
-                } else if(direction == "prev") {
-                    alert("첫 페이지입니다.");
-                    page = 1;
-                } else {
-                    alert("검색된 콘텐츠가 없습니다.");
-                }
+            if(data.totalCount < 1) {
+                alert("검색된 콘텐츠가 없습니다.");
+            }
 
-                activeBtn();
+            if(page < 1) {
+                alert("첫 페이지입니다.");
+                page = 1;
+            }
 
+            if(data.lastPage < page) {
+                alert("마지막 페이지 입니다.");
+                page = page - 1;
+            }
+
+            if(data.itemList.length < 1) {
                 return 0;
             }
 
-            $("#page").empty();
-            $("#page").append(page + " page");
-
             activeBtn();
 
-            return appendItem(data);
+            $("#page").empty();
+            $("#page").append(data.page + " page");
+
+            return appendItem(data.itemList);
         }
     );
 }
 
 function appendItem(data) {
+
+    console.log(data);
+
     var contentsList = $("#contentsList");
         contentsList.empty();
 
     for(var i=0; i<data.length; i++) {
 
-        var code = data[i].code;
+        var contentsId = data[i].contentsId;
         var title = data[i].title;
-        var titleEng = data[i].titleEng != "" ? "(" + data[i].titleEng + ")" : "";
         var year = data[i].year != "" ? "- " + data[i].year : "";
-        var director = data[i].director != "" ? data[i].director + " 감독 | " : "";
-        var genre = data[i].genre != "" ? data[i].genre + " | " : "";
-        var nation = data[i].nation != "" ? data[i].nation : "";
+        var actor = data[i].actor != null ? data[i].actor : "";
 
         var listItem = template
-        .replace("{code}", code)
-        .replace("{title}", title)
-        .replace("{titleEng}", titleEng)
-        .replace("{year}", year)
-        .replace("{director}", director)
-        .replace("{genre}", genre)
-        .replace("{nation}", nation);
+        .replace(TAG_CONTENTS_ID, contentsId)
+        .replace(TAG_TITLE, title)
+        .replace(TAG_ACTOR, actor)
+        .replace(TAG_YEAR, year);
 
         contentsList.append(listItem);
 
-        $("#" + code).on("click", setOnClickEvent);
+        $("#" + contentsId).on("click", setOnClickEvent);
     }
 
     return data.length;
@@ -136,11 +138,11 @@ function setOnClickEvent() {
 
     disableBtn();
 
-    var code = $(this).attr("id");
+    var contentsId = $(this).attr("id");
 
     $.post(
         URL_CONTENTS,
-        {code: code},
+        {contentsId: contentsId},
         function(data) {
 
             activeBtn();
