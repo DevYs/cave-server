3ec7d06fc5b9d14cca33171eadb2d80e64afc227
DevY <hanys1042@gmail.com>
2018-07-29 10:26:56 +0900

    세션 타임아웃 설정을 ServletContainerConfig.java로 옮김"

--- src/main/java/devy/cave/server/config/ServletContainerConfig.java
+++ src/main/java/devy/cave/server/config/ServletContainerConfig.java
@@ -6,19 +6,29 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
 
 @Component
 public class ServletContainerConfig implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
 
-    private final Logger logger = LoggerFactory.getLogger(ServletContainerConfig.class);
+    // Session Timeout 10 Hour
+    private final long SESSION_TIMEOUT_OF_MINUTE = 10 * 60;
 
     @Autowired
     private AdminConfigService adminConfigService;
 
     @Override
     public void customize(ConfigurableServletWebServerFactory factory) {
+        Duration between = Duration.ofMinutes(SESSION_TIMEOUT_OF_MINUTE);
+        Session session = new Session();
+        session.setTimeout(between);
+        factory.setSession(session);
+
         int port = Integer.valueOf(adminConfigService.getPort().getConfigValue());
         factory.setPort(port);
+
     }
 }
