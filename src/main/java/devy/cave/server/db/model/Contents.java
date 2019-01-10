fb20563b032e87c568a61488cf21a44af51ab101
DevY <hanys1042@gmail.com>
2019-01-10 12:45:27 +0900

    영화 콘텐츠 추가/수정시 youtube소개영상, 영화 클립 이미지, 다음 영화ID등의 추가정보를 입력하도록 하기 위한 form과 domain 클래스 수정

--- src/main/java/devy/cave/server/db/model/Contents.java
+++ src/main/java/devy/cave/server/db/model/Contents.java
@@ -51,6 +51,21 @@ public class Contents implements Serializable, MarshalledTupleKeyEntity {
 
     private String story;
 
+    /**
+     * 추가 입력 정보
+     */
+
+    // 유튜브 소개 영상 videoId
+    @NotNull
+    private String youtubeVideoId;
+
+    // 영화 클립 이미지
+    private String movieClipThumb;
+
+    // 다음 영화ID
+    @NotNull
+    private String daumMovieId;
+
     public Contents() {}
 
     public Contents(String channelNo, String contentsPosterUrl, String contentsName, String genre, String nation, String releaseDate, String runningTime, String director, String actor, String story) {
@@ -78,6 +93,23 @@ public class Contents implements Serializable, MarshalledTupleKeyEntity {
         this.director = director;
         this.actor = actor;
         this.story = story;
+    }
+
+    public Contents(String channelNo, String contentsNo, String contentsPosterUrl, String contentsName, String genre, String nation, String releaseDate, String runningTime, String director, String actor, String story, String youtubeVideoId, String movieClipThumb, String daumMovieId) {
+        this.channelNo = channelNo;
+        this.contentsNo = contentsNo;
+        this.contentsPosterUrl = contentsPosterUrl;
+        this.contentsName = contentsName;
+        this.genre = genre;
+        this.nation = nation;
+        this.releaseDate = releaseDate;
+        this.runningTime = runningTime;
+        this.director = director;
+        this.actor = actor;
+        this.story = story;
+        this.youtubeVideoId = youtubeVideoId;
+        this.movieClipThumb = movieClipThumb;
+        this.daumMovieId = daumMovieId;
     }
 
     public final String getChannelNo() {
@@ -168,11 +200,35 @@ public class Contents implements Serializable, MarshalledTupleKeyEntity {
         this.story = story;
     }
 
+    public String getYoutubeVideoId() {
+        return youtubeVideoId;
+    }
+
+    public void setYoutubeVideoId(String youtubeVideoId) {
+        this.youtubeVideoId = youtubeVideoId;
+    }
+
+    public String getMovieClipThumb() {
+        return movieClipThumb;
+    }
+
+    public void setMovieClipThumb(String movieClipThumb) {
+        this.movieClipThumb = movieClipThumb;
+    }
+
+    public String getDaumMovieId() {
+        return daumMovieId;
+    }
+
+    public void setDaumMovieId(String daumMovieId) {
+        this.daumMovieId = daumMovieId;
+    }
+
     @Override
     public String toString() {
         return "Contents{" +
-                "channelNo=" + channelNo +
-                ", contentsNo=" + contentsNo +
+                "channelNo='" + channelNo + '\'' +
+                ", contentsNo='" + contentsNo + '\'' +
                 ", contentsPosterUrl='" + contentsPosterUrl + '\'' +
                 ", contentsName='" + contentsName + '\'' +
                 ", genre='" + genre + '\'' +
@@ -182,6 +238,9 @@ public class Contents implements Serializable, MarshalledTupleKeyEntity {
                 ", director='" + director + '\'' +
                 ", actor='" + actor + '\'' +
                 ", story='" + story + '\'' +
+                ", youtubeVideoId='" + youtubeVideoId + '\'' +
+                ", movieClipThumb='" + movieClipThumb + '\'' +
+                ", daumMovieId='" + daumMovieId + '\'' +
                 '}';
     }
 
