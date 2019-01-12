152c71f5e9b0bdd42ee721a21652e681e47938ae
DevY <hanys1042@gmail.com>
2019-01-12 14:43:38 +0900

    상태 확인을 위한 페이지를 index.html에서 ping.html로 이름 변경

--- src/main/java/devy/cave/server/config/WebSecurityConfig.java
+++ src/main/java/devy/cave/server/config/WebSecurityConfig.java
@@ -63,7 +63,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/js/**", "/css/**", "/subtitle", "/index.html");
+        web.ignoring().antMatchers("/js/**", "/css/**", "/subtitle", "/ping.html");
     }
 
 }
