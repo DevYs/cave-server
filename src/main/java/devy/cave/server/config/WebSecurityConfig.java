ef43c3e945b110ca2921f99399673256b14d78ec
DevY <hanys1042@gmail.com>
2019-01-15 13:34:21 +0900

    불필요한 부분 제거

--- src/main/java/devy/cave/server/config/WebSecurityConfig.java
+++ src/main/java/devy/cave/server/config/WebSecurityConfig.java
@@ -64,7 +64,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/js/**", "/css/**", "/subtitle", "/ping.html", "/*.html", "/api/**");
+        web.ignoring().antMatchers("/js/**", "/css/**", "/subtitle", "/ping.html", "/api/**");
     }
 
 }
